90e70bf94488b8493cdaed9d1e46da5fd4f70b01
JerryYanTY <yan.tianyu@northeastern.edu>
2024-09-29 20:35:11 -0400

    Added solution

---
+++ diff_prod_avg.py
@@ -0,0 +1,33 @@
+'''
+    CS 5001
+    Lab 1
+'''
+
+'''
+Replace the # YOUR CODE HERE markers below with
+code that will calculate the correct values for
+difference, product, and average. Complete the
+print statements to output correctly formatted  
+results.
+
+Note: the code below is not syntactically correct and will not run until
+you replace the # YOUR CODE HERE with the correct solution.
+'''
+
+
+def main():
+    x = 5
+    y = 10
+
+    # Assign values to the variables below to calculate the difference, product, and average of the values in x and y.
+    difference = x - y
+    product = x * y
+    average = (x + y) / 2  # Get all the results of mathematical operations
+   
+    print(f'The difference between {x} and {y} is {difference}')
+    print(f'The product of {x} and {y} is {product}')
+    print(f'The average of {x} and {y} is {average}')  # Print results as specified in the requirements
+
+
+if __name__ == '__main__':
+    main()
---
+++ print_house.py
@@ -0,0 +1,30 @@
+'''
+    CS 5001
+    Lab 1
+'''
+
+'''
+Complete the code below so that the output looks as follows:
+      +
+     + +
+    +   +
+   +-----+
+   | .-. |
+   | | | |
+   +-+-+-+ 
+'''
+
+
+def main():
+    print('Here is a house!')
+    print('   +   ')
+    print('  + +  ')
+    print(' +   + ')
+    print('+-----+')
+    print('| .-. |')
+    print('| | | |')
+    print('+-+-+-+')
+
+
+if __name__ == '__main__':
+    main()
